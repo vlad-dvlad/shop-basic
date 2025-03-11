348fea03545b3188d414141e2cc975aa17a778da
Vladyslav Ivashko <vladyslavivashko@Vladyslavs-MacBook-Air.local>
2025-03-11 12:22:52 +0200

    base structue

---
+++ app/components/footer/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className='w-full bg-[var(--secondary)] p-4 text-center'>
+            <p className='text-lg font-bold color-[var(--text)]'>
+                @Vladyslav Ivashko
+            </p>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ app/components/header/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='w-full bg-[var(--accent)] p-4'>
+            <h1 className='text-lg font-bold color-[var(--text)]'>
+                Shop basic
+            </h1>
+        </header>
+    );
+};
+
+export default Header;
